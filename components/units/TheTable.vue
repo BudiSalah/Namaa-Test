1daaf1b46549acecce17edeeab315733a43c5ce8
Budi-Laptop <budi.salah@outlook.com>
2022-10-06 17:58:28 +0200

    create the main section in homepage without the popup

---
+++ components/units/TheTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="table-wrapper">
+    <table class="w-full" :class="[tableClasses]">
+      <thead>
+        <slot name="thead" />
+      </thead>
+
+      <tbody>
+        <slot name="tbody" />
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TheTable',
+  props: {
+    tableClasses: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,64 @@
 <template>
-  <Tutorial />
+  <main class="bg-gray-100">
+    <TheSection class="d-flex-col mt-6 gap-6 rounded-xl bg-white">
+      <section class="d-flex-row !items-stretch justify-end gap-3">
+        <TheBtn>
+          <SvgFilter
+            class="transition-colors duration-200 group-hover:fill-white"
+            :class="['transform']"
+          />
+        </TheBtn>
+
+        <TheBtn class="min-w-[132px]">
+          <SvgCirclePlus
+            class="transition-colors duration-200 group-hover:fill-white"
+            :class="['transform']"
+          />
+          <span>إضافة جديد</span>
+        </TheBtn>
+      </section>
+
+      <TheTable table-classes="min-w-[920px]">
+        <template #thead>
+          <tr>
+            <th>المورد</th>
+            <th>المورد</th>
+            <th>رقم الفاتورة</th>
+            <th>تاريخ الفاتورة</th>
+            <th>الاعدادت</th>
+          </tr>
+        </template>
+
+        <template #tbody>
+          <tr>
+            <td>اسم المورد</td>
+            <td>فلان الفلاني</td>
+            <td>SR 000001</td>
+            <td>01-01-2022</td>
+
+            <td>
+              <button>
+                <SvgEdit color="fill-black-100" />
+              </button>
+            </td>
+          </tr>
+
+          <tr>
+            <td>اسم المورد</td>
+            <td>فلان الفلاني</td>
+            <td>SR 000001</td>
+            <td>01-01-2022</td>
+
+            <td>
+              <button>
+                <SvgEdit color="fill-black-100" />
+              </button>
+            </td>
+          </tr>
+        </template>
+      </TheTable>
+    </TheSection>
+  </main>
 </template>
 
 <script>
