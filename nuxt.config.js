85a8038c3ee76d06334b9871714d247449dbed63
Budi-Laptop <budi.salah@outlook.com>
2022-10-06 13:26:11 +0200

    install postcss-import and cssnano

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,8 +51,10 @@ export default {
   build: {
     postcss: {
       plugins: {
+        'postcss-import': {},
         tailwindcss: {},
         autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
       },
     },
     loaders: {
